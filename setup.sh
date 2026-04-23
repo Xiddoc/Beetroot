da0f4ca8d3af095890bae6e70109d0b8d259d253
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2026-04-23 16:39:18 +0000

    Change flags for redroid

--- setup.sh
+++ setup.sh
@@ -8,16 +8,19 @@ git clone --depth 1 https://github.com/ayasa520/redroid-script.git /tmp/redroid
 
 # 2. Run the patcher using uv
 # -a 14.0.0: Android 14
-# -d: MindTheGapps (Recommended for Android 14)
 # -m: Magisk
 # -n: libndk (ARM translation for x86_64)
+# -i: Install houdini (translation)
+# -lg: LiteGapps
+# -mtg: MindTheGapps
 # -w: Widevine
+# Dont need Widevine for now & libndk only for And 11/12
 echo "[*] Patching Android 14 image (this may take a few minutes)..."
 cd /tmp/redroid
-uv run --with requests --with tqdm redroid.py -a 14.0.0 -g -m -n
+uv run --with requests --with tqdm redroid.py -a 14.0.0 -lg -i -m
 
 # Note: The script creates an image named:
-# redroid/redroid:14.0.0-mindthegapps-ndk-magisk
+# redroid/redroid:14.0.0-mindthegapps
 cd -
 
 # 3. Build the Research Layer
